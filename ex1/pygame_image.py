514bf6a4bc9d7c63b96a81090e28a2da64b309bc
luy869 <fyu.89600@gmail.com>
2025-06-10 16:35:36 +0900

    ファイル削除

--- ex1/pygame_image.py
+++ ex1/pygame_image.py
@@ -22,24 +22,28 @@ def main():
             if event.type == pg.QUIT: return
 
         key_lst = pg.key.get_pressed()
+        dx, dy = 0, 0
         if key_lst[pg.K_UP]:
-            kk_rct.move_ip((0, -1))
-        elif key_lst[pg.K_DOWN]:
-            kk_rct.move_ip((0, +1))
-        elif key_lst[pg.K_LEFT]:
-            kk_rct.move_ip((-1, 0)) 
-        elif key_lst[pg.K_RIGHT]:
-            kk_rct.move_ip((+1, 0)) 
+            dy -= 1
+        if key_lst[pg.K_DOWN]:
+            dy += 1
+        if key_lst[pg.K_LEFT]:
+            dx -= 1
+        if key_lst[pg.K_RIGHT]:
+            dx += 1
+        kk_rct.move_ip(dx, dy)
 
         screen.blit(bg_img, [-x, 0])
         screen.blit(bg_flip, [-x + 1600, 0])
         screen.blit(bg_img, [-x + 3200, 0])
-        screen.blit(kk_img, kk_rct)
+        kk_draw_rct = kk_rct.copy()
+        kk_draw_rct.x -= x
+        screen.blit(kk_img, kk_draw_rct)
 
         pg.display.update()
         tmr += 1     
         x += 1   
-        if x > 3199:
+        if x > 3200:
             x = 0 
         clock.tick(400)
 

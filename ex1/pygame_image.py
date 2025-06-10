5e8ad636559de68ea55f6f1a835dd3dfd35747de
luy869 <fyu.89600@gmail.com>
2025-06-10 15:45:25 +0900

    背景反転

--- ex1/pygame_image.py
+++ ex1/pygame_image.py
@@ -10,20 +10,21 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
+    bg_flip = pg.transform.flip(bg_img,True,False)
     kk_image = pg.image.load("fig/3.png")
     kk_image = pg.transform.flip(kk_image,True,False)
     tmr = 0
-    bg_x = 0
+    x = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [bg_x, 0])
-        screen.blit(bg_img, [bg_x + 800, 0])
+        screen.blit(bg_img, [x, 0])
+        screen.blit(bg_flip, [x + 1600, 0])
         screen.blit(kk_image,[300,200])
         pg.display.update()
         tmr += 1     
-        bg_x -= 1   
+        x -= 1   
         clock.tick(200)
 
 

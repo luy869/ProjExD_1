6a372dafa7871148607a161a5ea3fcba4751ccb0
luy869 <fyu.89600@gmail.com>
2025-06-10 16:08:49 +0900

    キー操作実装

--- ex1/pygame_image.py
+++ ex1/pygame_image.py
@@ -11,18 +11,31 @@ def main():
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
     bg_flip = pg.transform.flip(bg_img,True,False)
-    kk_image = pg.image.load("fig/3.png")
-    kk_image = pg.transform.flip(kk_image,True,False)
+    kk_img = pg.image.load("fig/3.png")
+    kk_img = pg.transform.flip(kk_img,True,False)
+    kk_rct = kk_img.get_rect()
+    kk_rct.center = 300, 200
     tmr = 0
     x = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
+        key_lst = pg.key.get_pressed()
+        if key_lst[pg.K_UP]:
+            kk_rct.move_ip((0, -1))
+        elif key_lst[pg.K_DOWN]:
+            kk_rct.move_ip((0, +1))
+        elif key_lst[pg.K_LEFT]:
+            kk_rct.move_ip((-1, 0)) 
+        elif key_lst[pg.K_RIGHT]:
+            kk_rct.move_ip((+1, 0)) 
+
         screen.blit(bg_img, [-x, 0])
         screen.blit(bg_flip, [-x + 1600, 0])
         screen.blit(bg_img, [-x + 3200, 0])
-        screen.blit(kk_image,[300,200])
+        screen.blit(kk_img, kk_rct)
+
         pg.display.update()
         tmr += 1     
         x += 1   

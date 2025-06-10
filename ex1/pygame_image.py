50f4e5ad246e175fda5d0412f741ac414552d176
luy869 <fyu.89600@gmail.com>
2025-06-10 15:30:28 +0900

    こうかとん移動

--- ex1/pygame_image.py
+++ ex1/pygame_image.py
@@ -13,15 +13,17 @@ def main():
     kk_image = pg.image.load("fig/3.png")
     kk_image = pg.transform.flip(kk_image,True,False)
     tmr = 0
+    bg_x = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [0, 0])
+        screen.blit(bg_img, [bg_x, 0])
         screen.blit(kk_image,[300,200])
         pg.display.update()
-        tmr += 1        
-        clock.tick(10)
+        tmr += 1     
+        bg_x -= 1   
+        clock.tick(200)
 
 
 if __name__ == "__main__":
